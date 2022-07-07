3bfab01d59fd137731bdcc376d38279cb0e6b2a9
HafizhFaza <hafizhfazanugraha@gmail.com>
2022-07-07 16:07:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Task 1
+
+var bilangan = prompt("input bilangan");
+var pembagi = prompt("input pembagi");
+var kuadrat = prompt("input kuadrat");
+
+let hasil_bagi = bilangan / pembagi;
+let hasil_kuadrat = Math.pow(bilangan, kuadrat);
+
+alert(`Hasil Bagi: ${hasil_bagi} dan Hasil Kuadrat: ${hasil_kuadrat}`);
+
+// Task 3
+
+// let now = new Date();
+// now = now.toDateString();
+// console.log(now);
+
+// let kemarin = new Date(now);
+// kemarin.setDate(kemarin.getDate() - 1);
+// console.log(kemarin.toDateString());
+
+// let besok = new Date(now);
+// besok.setDate(besok.getDate() + 1);
+// console.log(besok.toDateString());
+
+// Task 5
+// var kalimat = prompt("Input Kalimat");
+// let count = kalimat.length;
+// alert(`Jumlah Huruf: ${count}`);
